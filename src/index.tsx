e4e371b8337e56b286fbc145cf8990c6b24fbd22
olafpd <olafpd@umich.edu>
2024-12-02 11:08:29 -0500

    undo hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router";
 import App from "./App";
 
 // ...existing code...
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
